3e5eedd016c27c70d8b37a59b62e8ba6f7011977
Ashlie Gray <ashliejanegray@gmail.com>
2020-09-25 12:45:51 +1000

    added default users and patient csv files, converted controlflow to png to send to glen

--- Documents/assessment/AshlieGray _T1A3_2/src/main.rb
+++ Documents/assessment/AshlieGray _T1A3_2/src/main.rb
@@ -5,6 +5,7 @@ require 'csv'
 
 
 
+
 #classes and methods
 class User
     attr_reader :username, :password
@@ -13,20 +14,20 @@ class User
         @password = password
         @patient_log = []
     end
-    def read_log 
-        @patient_log = []
-        File.foreach("#{@patient_name}.txt") {|line| @patient_log.push(line.chomp)}
+    # def read_log 
+    #     @patient_log = []
+    #     File.foreach("#{@patient_name}.txt") {|line| @patient_log.push(line.chomp)}
         
-    end
+    # end
 
-    def add_log(log)
-        File.write("#{@patient_name}.txt", mode: "a")
-    end
+    # def add_log(log)
+    #     File.write("#{@patient_name}.txt", mode: "a")
+    # end
 end
 
 class Patient
     attr_reader :first_name, :last_name, :species, :breed, :age, :sex
-    def initialize(first_name, last_name)
+    def initialize(full_name, species)
         @first_name = first_name
         @last_name = last_name
         @species =species
@@ -34,25 +35,38 @@ class Patient
         @age = age
         @sex = sex
     end
-    def read_log 
-        @patient_log = []
-        File.foreach("#{@patient_name}.txt") {|line| @patient_log.push(line.chomp)}
+    # def read_log 
+    #     @patient_log = []
+    #     File.foreach("#{@patient_name}.txt") {|line| @patient_log.push(line.chomp)}
         
-    end
+    # end
 
-    def add_log(log)
-        File.write("#{@patient_name}.txt", mode: "a")
-    end
+    # def add_log(log)
+    #     File.write("#{@patient_name}.txt", mode: "a")
+    # end
 end
 
 #databases
 user_database = []
+begin
 CSV.foreach("Users.csv", headers: true) do |row| 
-    user_database << User.new(row["username"], row["password"])
+user_database << User.new(row["username"], row["password"])
+rescue 
+    puts "Database, empty, create a user"
+    
 end
+
+end
+
 patient_database = []
+begin
 CSV.foreach("Patients.csv", headers: true) do |row| 
-    patient_database << Patient.new(row["username"], row["password"])
+patient_database << Patient.new(row["patient_full_name"], row["species"])
+rescue 
+    puts "patient database empty, please add a patient"
+    
+end
+
 end
 
 #opening menu
@@ -68,21 +82,20 @@ menu.choice "Create Account"
 menu.choice "Exit"   
 end
 user = nil
+patient = nil
 
     loop do    
         if  welcome == "Create Account"
         user_name= prompt.ask("Enter a username")
         user_password = prompt.mask("Enter a password")
-        user = User.new(user_name, user_password)
+        user = User.new(user.user_name, user.user_password)
         user_database << user
-        CSV.open("Users.csv", "a") do |csv| 
-            csv << ["#{user_name}", "#{user_password}"]   
-            end    
-            puts "Welcome #{user_name}, you are now logged in"
+        CSV.open("Users.csv", "a") { |csv| csv << ["#{user_name}", "#{user_password}"] } 
+            puts "Welcome #{user.user_name}, you are now logged in"
 
             elsif welcome == "Login"
             username = prompt.ask("Enter username")
-            current_user = user_database.find{ |user| user == username}
+            current_user = user_database.find{ |user| user.username == username}
                 if username == current_user.username
                 password = prompt.mask('Enter your password')
                 if password == current_user.password
@@ -106,30 +119,41 @@ user = nil
             menu.choice "Exit"   
         end
 
-        if main_menu == "Find Patient"
-            patient_first_name= prompt.ask("Patient First Name")
-            patient_last_name= prompt.ask("Patient Last Name")
-            #search patient data base
-            # begin
-            #     user.read_log
-            # rescue
-            #     puts "You haven't added any notes yet!"
-            # end
+        if main_menu == "Find Patient" && patient == nil
+            patient_full_name= prompt.ask("Patient Full Name")
+            current_patient = patient_database.find{ |patient| patient == patient_full_name}
+                if patient_full_name == current_patient
+                   patient == current_patient
+                else
+                puts "Access Denied"
+                end 
+                if patient != nil
+                    patient_menu = prompt.select("Would you like to add log or read log?") do |menu|
+                        menu.choice "Add"
+                        menu.choice "Read"
+                        menu.choice "Exit"   
+                        end 
+                        if patient_menu == "Add"
+                        new_log = prompt.ask("type your log")
+                        add_log(new_log)
+                        File.write("#{@patient_name}.txt", mode: "a")
+                        end
+                    end
+                break
         elsif main_menu == "Add Patient"
-                patient_first_name= prompt.ask("Patient First Name")
-                patient_last_name= prompt.ask("Patient Last Name")
+                patient_full_name= prompt.ask("Patient Full Name")
                 patient_species= prompt.ask("Patient Species")
                 patient_breed= prompt.ask("Patient Breed")
                 patient_age= prompt.ask("Patient Age")
                 patient_sex= prompt.ask("Patient Sex")
-                patient = Patient.new(patient_first_name, patient_last_name)
+                patient = Patient.new(patient_full_name, patient_species)
                 patient_database << patient
                 CSV.open("Patients.csv", "a") do |csv| 
-                    csv << ["#{patient_first_name}", "#{patient_last_name}", "#{patient_species}", "#{patient_breed}", "#{patient_age}", "#{patient_sex}"]   
+                    csv << ["#{patient_full_name}", "#{patient_species}", "#{patient_breed}", "#{patient_age}", "#{patient_sex}"]   
                 end    
-                puts "Thank you, #{patient_first_name} has been added to the system"
-        elsif input == "Exit"
-            puts "Thank you #{user.username}, you have now logged out"
+                puts "Thank you, #{patient_full_name} has been added to the system"
+        elsif patient_menu == "Exit"
+            puts "Thank you #{username}, you have now logged out"
             user = nil
             system("clear")
             puts ""
@@ -139,3 +163,4 @@ user = nil
         end
     end
 end
+
