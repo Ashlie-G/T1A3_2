366d9c489f7ccc1d36050b6f7b4a81a5410af5d5
Ashlie Gray <ashliejanegray@gmail.com>
2020-09-28 14:21:25 +1000

    csv file issue fized and workinggit add .

--- Documents/assessment/AshlieGray _T1A3_2/src/main2.rb
+++ Documents/assessment/AshlieGray _T1A3_2/src/main2.rb
@@ -26,37 +26,29 @@ class Patient
         @age = age
         @sex = sex
         @patient_log
-        
     end
-
-   
 end
-@patient_log = []
 
-def view_log(patient) 
-# log_table = TTY::Table.new
-#  CSV.foreach(".csv", headers :true) do |row|
-#     log_table << row
-#     log_table << row
-#  end
-#     puts log_table.render(:ascii)
-#    log_table = TTY::Table.new
-#    log_table << @patient_log
-#    puts log_table.render(:ascii)    
+@patient_log = []
+#attempt to move data to csv's
+def view_log 
+    @patient_log =CSV.parse(File.read(".csv"), headers: true)
+    #    log_table = TTY::Table.new
+    #    log_table << @patient_log
+    #    puts log_table.render(:ascii)
+    puts @patient_log
 #    log_table = TTY::Table.new(["Temperature", "Pulse", "Resp Rate"],[[temperature, pulse, respiration]])
 #     puts log_table.render(:ascii)
-
-    @patient_log =CSV.parse(File.read("#{patient}.csv"), headers: true)
-    puts @patient_log
+   
 
 end
 
-def add_log(patient, temperature, pulse, respiration)
-    @patient_log = temperature, pulse, respiration
-    CSV.open("#{patient}.csv", "a") do |csv|
-    csv << ["Temp", "Pulse", "Resp Rate"]
-    csv << [temperature, pulse, respiration]
-    csv << ["", Time.now]
+def add_log(temperature, pulse, respiration)
+        @patient_log = temperature, pulse, respiration
+    CSV.open("#{full_name}.csv", "a") do |csv|
+        csv << ["Temp", "Pulse", "Resp Rate"]
+        csv << [temperature, pulse, respiration]
+        csv << ["#{@username}", Time.now]
     end   
 end
 
@@ -83,6 +75,7 @@ end
 
 title_screen
 #opening menu
+prompt = TTY::Prompt.new
 prompt = TTY::Prompt.new(active_color: :magenta)
 
 welcome = prompt.select("Login or Create New") do |menu|
@@ -90,12 +83,10 @@ menu.choice "Login"
 menu.choice "Create Account"
 menu.choice "Exit"   
 end
-
 user = nil
-patient = nil 
+patient = nil
 
-    loop do   
-        
+    loop do    
         if  welcome == "Create Account"
             user_name= prompt.ask("Enter a username")
             user_password = prompt.mask("Enter a password")
@@ -111,7 +102,7 @@ patient = nil
                         if username == current_user.username
                             password = prompt.mask('Enter your password')
                             if password == current_user.password
-                                user = current_user
+                                user == current_user
                                 puts "Welcome #{current_user.username}".colorize(:magenta)
                             else
                                 puts "Invalid username or password".colorize(:red)
@@ -149,8 +140,8 @@ patient = nil
                 patient_full_name= prompt.ask("Patient Full Name")
                 current_patient = patient_list.find{ |patient| patient.full_name == patient_full_name}
                 begin    
-                    if patient_full_name = current_patient.full_name
-                       patient = current_patient.full_name
+                    if patient_full_name == current_patient.full_name
+                       patient == current_patient
                         loop do
                             patient != nil
                             patient_menu = prompt.select("Would you like to add log or view log?") do |menu|
@@ -159,27 +150,24 @@ patient = nil
                             menu.choice "Help (Normal Ranges)"
                             menu.choice "Exit Patient Menu"   
                             end 
-                        
+                      
                             if  patient_menu == "Add"
                                 temp = prompt.ask("Temperature")
                                 pulse = prompt.ask("Pulse")
                                 resprate = prompt.ask("Resp")
-                                patient = current_patient.full_name
-                                add_log(patient, temp, pulse, resprate)
-                
+                                add_log(temp, pulse,resprate)
+                        
                             elsif patient_menu == "View"
-                                patient = current_patient.full_name
-                                view_log(patient)
+                              view_log
 
                             elsif patient_menu == "Help (Normal Ranges)"
                                 puts "Normal ranges are:"
                                 normalrange = TTY::Table.new(["Dog","Cat","Guinea Pig"], [["Temp - 38.4-39.1", "Temp - 38.2-38.6", "Temp -37.2 -39.5"], ["Pulse - 60-180 bpm", "Pulse - 120-220 bpm", "Pulse - 230 -380 bpm"], ["Resp Rate - 10-30 brpm", "Resp Rate - 24-42 brpm", "Resp Rate -42 -104brpm"]])
                                 puts normalrange.render(:ascii)
                                 puts "bpm = beats per minute\nbrpm = breaths per minute"
-                            break
                             else patient_menu == "Exit Patient Menu"
-                                 patient = nil
-                        break
+                                
+                            break
                             
                     
                             end
@@ -206,9 +194,9 @@ patient = nil
                 puts "Thank you, #{patient_full_name} has been added to the system".colorize(:magenta)
             
             elsif main_menu == "Help"
-                puts "The TPRP Monitor allows you to easily log the temperature, pulse and rate\nrespiration rate of your patients"
+                puts "The TPRP Monitor allows you to easily log the temperature, pulse rate\nrespiration rate and pain score of your patients"
                 puts "Use the up and down arrows to navigate the menu"
-                   
+            break       
 
             else main_menu == "Exit"
                 puts "Thank you #{current_user.username}, you have now logged out".colorize(:magenta)
@@ -219,4 +207,3 @@ patient = nil
        
          end
     end    
-
