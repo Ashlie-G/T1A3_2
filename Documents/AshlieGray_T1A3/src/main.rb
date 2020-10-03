3b11a0d576b6cd1faa165d4390fc40ab16eba06b
Ashlie Gray <ashliejanegray@gmail.com>
2020-10-04 09:49:18 +1000

    test run of error handling, added commenting to main rb

--- Documents/AshlieGray_T1A3/src/main.rb
+++ Documents/AshlieGray_T1A3/src/main.rb
@@ -9,12 +9,11 @@ require_relative 'methods'
 # databases
 user_list = []
 patient_list = []
-@patient_log = []
 
 # Main
 title_screen
 prompt = TTY::Prompt.new(active_color: :magenta, interrupt: :exit)
-
+#user database file
 begin
   CSV.foreach('Users.csv', headers: true) do |row|
     user_list << User.new(row['username'], row['password'])
@@ -25,7 +24,7 @@ end
 
 patient = nil
 user = nil
-
+#opening menu
 loop do
   welcome = prompt.select('Login or Create New', help: '(Case Sensative)', show_help: :always) do |menu|
     menu.choice 'Login'
@@ -34,7 +33,7 @@ loop do
   end
   system("clear")
   title_screen
-
+#create new user
   if welcome == 'Create Account'
     user_name = prompt.ask('Enter a username', required: true)
     user_password = prompt.mask('Enter a password', required: true)
@@ -45,7 +44,7 @@ loop do
     sleep 1.5
     system("clear")
     title_screen
-
+#user login
   elsif welcome == 'Login'
     begin
     username = prompt.ask('Enter username', required: true)
@@ -70,7 +69,7 @@ loop do
   sleep 1.5
     system("clear")
     title_screen
-
+#Exit app
   elsif welcome == 'Exit'
     puts 'Thankyou for using the TPR Tracker'.colorize(:magenta)
     user = nil
@@ -79,7 +78,7 @@ loop do
     puts 'invalid Input'.colorize(:red)
     break
   end
-
+#patient database file
   begin
     CSV.foreach('Patients.csv', headers: true) do |row|
       patient_list << Patient.new(row['first_name'], row['last_name'], row['species'], row['breed'], row['age'], row['sex'])
@@ -87,7 +86,7 @@ loop do
   rescue (Errno::ENOENT)
     puts "No patient's on file, please create patient profile".colorize(:red)
   end
-
+#main menu
   loop do
     main_menu = prompt.select('What would you like to do?', help: '(Case Sensative)', show_help: :always) do |menu|
       menu.choice 'Find Patient'
@@ -95,7 +94,7 @@ loop do
       menu.choice 'Help'
       menu.choice 'Exit'
     end
-
+#find/search for patient
     if main_menu == 'Find Patient'
       patient_name = prompt.ask('Patient Name', required: true)
       current_patient = patient_list.find { |patient| patient.first_name == patient_name }
@@ -106,6 +105,7 @@ loop do
             patient = current_patient
             puts "#{current_patient.first_name} #{current_patient.last_name} is #{current_patient.species}, is a #{current_patient.breed}, is #{current_patient.age} years old and is #{current_patient.sex}".colorize(:light_blue)
             loop do
+#patient menu
               !patient.nil?
               patient_menu = prompt.select('Would you like to add log or view log?') do |menu|
                 menu.choice 'Add'
@@ -113,7 +113,7 @@ loop do
                 menu.choice 'Help (Normal Ranges)'
                 menu.choice 'Exit Patient Menu'
               end
-
+#add to patient log
               if patient_menu == 'Add'
                 temp = prompt.ask('Temperature', convert: :float, required: true)
                 pulse = prompt.ask('Pulse', convert: :int, required: true)
@@ -128,12 +128,13 @@ loop do
                 rescue StandardError
                   puts 'no record yet, please add log'.colorize(:red)
                   end
+#patient help menu                  
               elsif patient_menu == 'Help (Normal Ranges)'
                 puts 'Normal ranges are:'
                 normalrange = TTY::Table.new(['Dog', 'Cat', 'Guinea Pig'], [['Temp - 38.4-39.1', 'Temp - 38.2-38.6', 'Temp -37.2 -39.5'], ['Pulse - 60-180 bpm', 'Pulse - 120-220 bpm', 'Pulse - 230 -380 bpm'], ['Resp Rate - 10-30 brpm', 'Resp Rate - 24-42 brpm', 'Resp Rate -42 -104brpm']])
                 puts normalrange.render(:ascii)
                 puts "bpm = beats per minute\nbrpm = breaths per minute".colorize(:magenta)
-
+#exit patient menu
               else patient_menu == 'Exit Patient Menu'
                    patient = nil
                    break
@@ -149,10 +150,12 @@ loop do
         end
       rescue StandardError
         puts 'Patient not found, please try again or add a new patient file'.colorize(:red)
+        sleep 1.5
       end
       sleep 0.5
       system("clear")
       title_screen
+#create new patient file      
     elsif main_menu == 'Add Patient'
       patient_name = prompt.ask('Patient First Name', required: true)
       patient_last_name = prompt.ask('Patient Last Name', required: true)
@@ -169,10 +172,12 @@ loop do
       sleep 1.5
       system("clear")
       title_screen
+#help menu
     elsif main_menu == 'Help'
       puts 'The TPRP Monitor allows you to easily log the temperature, pulse and respiration rate of your patients'.colorize(:magenta)
       puts 'Use the up and down arrows to navigate the menu'.colorize(:light_blue)
-      puts 'Press control + c to cancel running the app at anytime'.colorize(:light_blue)  
+      puts 'Press control + c to cancel running the app at anytime'.colorize(:light_blue)
+#exit/logout of app        
     else main_menu == 'Exit'
          puts "Thank you #{current_user.username}, you have now logged out".colorize(:magenta)
          sleep 1
